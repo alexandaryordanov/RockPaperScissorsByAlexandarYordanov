2ce6df621cce826348d295b97c2e0324ca2de616
Alexandar Yordanov <anonimovbg@gmail.com>
2023-05-21 21:35:31 +0300

    Final version!

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,31 @@
+import random
 # User input
-
-
+rock = "Rock"
+paper = "Paper"
+scissors = "Scissors"
+player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 # Logic
-
+if player_move == "r":
+    player_move = rock
+elif player_move == "p":
+    player_move = paper
+elif player_move == "s":
+    player_move = scissorsCompleted
+else:
+    raise SystemExit("Invalid Input. Try again...")
+computer_random_number = random.randint(1, 3)
+computer_move = ""
+if computer_random_number == 1:
+    computer_move = rock
+elif computer_random_number == 2:
+    computer_move = paper
+elif computer_random_number == 3:
+    computer_move = scissors
 # Output
+print(f"The computer chose {computer_move}.")
+if player_move == computer_move:
+    print("Draw!")
+elif (player_move == rock and computer_move == paper) and (player_move == paper and computer_move == scissors) and (player_move == scissors and computer_move == rock):
+    print("You lose!")
+else:
+    print("You win!")
